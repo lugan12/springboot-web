1211f97c1b7abffce756ebb6ab11ce9308a40c6a
lugan_gbl <1261051142@qq.com>
2018-08-12 13:25:46 +0800

    首次修改提交

--- src/main/java/com/example/springboot/MyTest.java
+++ src/main/java/com/example/springboot/MyTest.java
@@ -2,6 +2,6 @@ package com.example.springboot;
 
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("老子来也");
+        System.out.println("111老子来也");
     }
 }
